761d725c19bcc6b3788bedbb3a22cc0caaa4f2a7
Alec Heifetz <aheifetz@mit.edu>
2014-10-04 17:24:04 -0400

    Fixed twilio

--- twilio.js
+++ twilio.js
@@ -3,8 +3,8 @@ var twilio = {};
 var express = require('express');
 var Twilio = require('twilio');
 
-var TWILIO_ACCOUNT_SID = 'Your account here';
-var TWILIO_AUTH_TOKEN = 'Your token here';
+var TWILIO_ACCOUNT_SID = 'AC6be2a1414ab8bd83a22db24e91db6279';
+var TWILIO_AUTH_TOKEN = '223bce8a531c574d21c3ca3e78f385f0';
 
 var client = new Twilio.RestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
@@ -12,12 +12,6 @@ function processResponse = function(req, res) {
     var text = req.body.Body;
     var responseText = getSpitzersShit(text);
 
-    client.sms.messages.create({
-      to: user.phone,
-      from:'+13132087874',
-      body:'You have entered a high crime zone! Be careful.'
-    });
-    twilio.send()
     var from = req.body.From;
 
 }
