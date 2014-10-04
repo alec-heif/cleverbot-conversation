f2fe953ab1399f676259608fa5b34aa10f84fa6e
Alec Heifetz <aheifetz@mit.edu>
2014-10-04 17:26:41 -0400

    Install python

--- twilio.js
+++ twilio.js
@@ -3,6 +3,8 @@ var twilio = {};
 var express = require('express');
 var Twilio = require('twilio');
 
+var nodePython = require('node-python');
+
 var TWILIO_ACCOUNT_SID = 'AC6be2a1414ab8bd83a22db24e91db6279';
 var TWILIO_AUTH_TOKEN = '223bce8a531c574d21c3ca3e78f385f0';
 
