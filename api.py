1e49819fd33497451246c0cb3c4c2082910b9dd2
William Spitzer <spitzerw@mit.edu>
2014-10-04 18:04:32 -0400

    fixing api

--- api.py
+++ api.py
@@ -1,20 +1,13 @@
 from chatterbotapi import ChatterBotFactory, ChatterBotType
 # Download the twilio-python library from http://twilio.com/docs/libraries
 #from twilio.rest import TwilioRestClient
+import sys
 from random import randint
-
-# function getResponse:
-# Takes in an input message and a session and returns cleverbot message based on that session
-def getResponse(inputMSG, botsession):
-    print 'input> ' + inputMSG
-    s = botsession.think(inputMSG);
-    print 'bot1> ' + s
-    return s
 	
 # function getSession:
 # creates a new cleverbot factory and returns it as a session
 def getSession():
-	factory = ChatterBotFactory()
+    factory = ChatterBotFactory()
     rInt = randint(0,1)
     if rInt == 1:
         bot1 = factory.create(ChatterBotType.CLEVERBOT)
@@ -22,3 +15,9 @@ def getSession():
         bot1 = factory.create(ChatterBotType.PANDORABOTS, 'b0dafd24ee35a477')
     
 	return bot1.create_session()
+	
+	
+botSession = getSession()
+
+for line in sys.stdin:
+    return botSession.think(line)
