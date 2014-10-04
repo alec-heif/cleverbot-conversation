f12ab726d7c8a5ae78e3d56933a970ee2b6cd7b8
William Spitzer <spitzerw@mit.edu>
2014-10-04 17:12:56 -0400

    added the cleverbot stuff in python

--- api.py
+++ api.py
@@ -1,19 +1,24 @@
 from chatterbotapi import ChatterBotFactory, ChatterBotType
+# Download the twilio-python library from http://twilio.com/docs/libraries
+#from twilio.rest import TwilioRestClient
+from random import randint
 
-factory = ChatterBotFactory()
-
-bot1 = factory.create(ChatterBotType.CLEVERBOT)
-bot1session = bot1.create_session()
-
-bot2 = factory.create(ChatterBotType.PANDORABOTS, 'b0dafd24ee35a477')
-bot2session = bot2.create_session()
-
-s = 'Hi'
-while (1):
-    
+# function getResponse:
+# Takes in an input message and a session and returns cleverbot message based on that session
+def getResponse(inputMSG, botsession):
+    print 'input> ' + inputMSG
+    s = botsession.think(inputMSG);
     print 'bot1> ' + s
+    return s
+	
+# function getSession:
+# creates a new cleverbot factory and returns it as a session
+def getSession():
+	factory = ChatterBotFactory()
+    rInt = randint(0,1)
+    if rInt == 1:
+        bot1 = factory.create(ChatterBotType.CLEVERBOT)
+    else:
+        bot1 = factory.create(ChatterBotType.PANDORABOTS, 'b0dafd24ee35a477')
     
-    s = bot2session.think(s);
-    print 'bot2> ' + s
-    
-    s = bot1session.think(s);
+	return bot1.create_session()
