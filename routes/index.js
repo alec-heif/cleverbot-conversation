a89f5f7200f94ede21872d092ddfc4550ea0991b
Peinan Chen <peinanchen@gmail.com>
2014-10-04 23:25:01 -0400

    Moar

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var Twilio = require('twilio');
-var SIDS = {'+13132087874':'AC6be2a1414ab8bd83a22db24e91db6279', '+12406882879':'AC1ebb3cd2cd6ab8a2e466ca4158bcc6af', '14423336273': 'AC85e3a1f973eed48e25413f64e880ac65'}
-var TOKENS = {'+13132087874':'223bce8a531c574d21c3ca3e78f385f0', '+12406882879':'591c1714512b04ac507357fbde0c23f3', '14423336273':'ba5fedf59c42e20d1065148f23426e3a'}
+var SIDS = {'+13132087874':'AC6be2a1414ab8bd83a22db24e91db6279', '+12406882879':'AC1ebb3cd2cd6ab8a2e466ca4158bcc6af', '+14423336273': 'AC85e3a1f973eed48e25413f64e880ac65'}
+var TOKENS = {'+13132087874':'223bce8a531c574d21c3ca3e78f385f0', '+12406882879':'591c1714512b04ac507357fbde0c23f3', '+14423336273':'ba5fedf59c42e20d1065148f23426e3a'}
 
 var Cleverbot = require('../cleverbot');
 
