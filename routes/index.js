cde72e70894aaf8dbaf33bbb0b3641522b756c5e
Alec Heifetz <aheifetz@mit.edu>
2014-10-04 22:46:28 -0400

    Finished cleverbot

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var TWILIO_ACCOUNT_SID = 'AC6be2a1414ab8bd83a22db24e91db6279';
 var TWILIO_AUTH_TOKEN = '223bce8a531c574d21c3ca3e78f385f0';
 var client = new Twilio.RestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-var Cleverbot = require('cleverbot-node');
+var Cleverbot = require('../cleverbot');
 
 var threads = {};
 
