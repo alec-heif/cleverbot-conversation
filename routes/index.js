564cd2e4d6714756cf6a1b936c710a8d6e32a0d6
Peinan Chen <peinanchen@gmail.com>
2014-10-04 23:40:43 -0400

    Added numbers

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var Twilio = require('twilio');
-var SIDS = {'+13132087874':'AC6be2a1414ab8bd83a22db24e91db6279', '+12405472780':'AC1ebb3cd2cd6ab8a2e466ca4158bcc6af', '+14423336273': 'AC85e3a1f973eed48e25413f64e880ac65'}
-var TOKENS = {'+13132087874':'223bce8a531c574d21c3ca3e78f385f0', '+12405472780':'591c1714512b04ac507357fbde0c23f3', '+14423336273':'ba5fedf59c42e20d1065148f23426e3a'}
+var NUMBERS = ['3132087874', '2405472780', '4423336273']
+var SIDS = {'3132087874':'AC6be2a1414ab8bd83a22db24e91db6279', '2405472780':'AC1ebb3cd2cd6ab8a2e466ca4158bcc6af', '4423336273': 'AC85e3a1f973eed48e25413f64e880ac65'};
+var TOKENS = {'3132087874':'223bce8a531c574d21c3ca3e78f385f0', '2405472780':'591c1714512b04ac507357fbde0c23f3', '4423336273':'ba5fedf59c42e20d1065148f23426e3a'};
 
 var Cleverbot = require('../cleverbot');
 
@@ -20,6 +21,8 @@ router.get('/', function(req, res) {
     var message = req.query.Body;
     var from = req.query.From;
     var to = req.query.To;
+    console.log(to);
+    console.log(SIDS[to]);
     var client = new Twilio.RestClient(SIDS[to], TOKENS[to]);
 
     function callback(resp) {
