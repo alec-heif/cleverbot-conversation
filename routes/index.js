e8e80c101eb162800ff5de74b8616a6f968b5c3d
Alec Heifetz <aheifetz@mit.edu>
2014-10-04 22:28:22 -0400

    Converted to node module

--- routes/index.js
+++ routes/index.js
@@ -6,58 +6,31 @@ var TWILIO_ACCOUNT_SID = 'AC6be2a1414ab8bd83a22db24e91db6279';
 var TWILIO_AUTH_TOKEN = '223bce8a531c574d21c3ca3e78f385f0';
 var client = new Twilio.RestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-var PythonShell = require('python-shell');
+var Cleverbot = require('cleverbot-node');
 
 var threads = {};
 
-function sendMessage(message, number) {
-    console.log(message, number);
-
-    client.sms.messages.create({
-      to: number,
-      from: '+13132087874',
-      body: message
-    });
-};
-
 function killProcess(process, from) {
-    process.kill('SIGINT');
     delete threads[from];
 };
 
 router.get('/', function(req, res) {
+    if(!req.query.Body || !req.query.From) {
+        return;
+    }
     var message = req.query.Body;
     var from = req.query.From;
-    if (threads[from]) {
-        var thread = threads[from];
-        clearTimeout(thread.killer);
-        thread.killer = setTimeout(
-            function() {
-                killProcess(thread.process, from);
-            }, 
-            300000
-        );
-        thread.process.stdin.write(message);
+
+    function callback(resp) {
+        client.sms.messages.create({
+            to: from,
+            from: '+13132087874',
+            body: resp.message
+        });
     }
-    else {
-        console.log("in else", message, from);
-        var process = new PythonShell('api.py');
-        process.send(message);
-        process.on('message', function(message) {
-            console.log(message);
-            sendMessage(message, from);
-        })
-        threads[from] = {
-            process: process, 
-            killer: setTimeout(
-                function() {
-                    killProcess(process, from);
-                }, 
-                300000
-            )
-        };
-        console.log('Exited');
-    }
+
+    threads[from] = threads[from] || new Cleverbot();
+    thread.write(message, callback);
     res.render('index/index');
 });
 
