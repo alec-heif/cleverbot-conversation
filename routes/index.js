9617097f3b8b7ebeaa150178e6da8ed40c015ca2
Peinan Chen <peinanchen@gmail.com>
2014-10-04 23:19:23 -0400

    Added multiple users

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var Twilio = require('twilio');
-var TWILIO_ACCOUNT_SID = 'AC6be2a1414ab8bd83a22db24e91db6279';
-var TWILIO_AUTH_TOKEN = '223bce8a531c574d21c3ca3e78f385f0';
+var SIDS = {'+13132087874':'AC6be2a1414ab8bd83a22db24e91db6279', '+12406882879':'AC1ebb3cd2cd6ab8a2e466ca4158bcc6af', '14423336273': 'AC85e3a1f973eed48e25413f64e880ac65'}
+var TOKENS = {'+13132087874':'223bce8a531c574d21c3ca3e78f385f0', '+12406882879':'591c1714512b04ac507357fbde0c23f3', '14423336273':'ba5fedf59c42e20d1065148f23426e3a'}
 var client = new Twilio.RestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 var Cleverbot = require('../cleverbot');
@@ -20,17 +20,19 @@ router.get('/', function(req, res) {
     }
     var message = req.query.Body;
     var from = req.query.From;
+    var to = req.query.To;
+    var client = new Twilio.RestClient(SIDS[to], TOKENS[to]);
 
     function callback(resp) {
         client.sms.messages.create({
             to: from,
-            from: '+13132087874',
+            from: to,
             body: resp.message
         });
     }
 
-    threads[from] = threads[from] || new Cleverbot();
-    threads[from].write(message, callback);
+    threads[to+from] = threads[to+from] || new Cleverbot();
+    threads[to+from].write(message, callback);
     res.render('index/index');
 });
 
